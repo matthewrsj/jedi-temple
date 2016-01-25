f52efb24625771ead59ce8665b6b2702df24953b
Cody Malick <cody.malick@gmail.com>
2016-01-25 01:12:42 -0800

    Fixed typo

--- header.php
+++ header.php
@@ -22,7 +22,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">Jedit Temple</a>
+      <a class="navbar-brand" href="#">Jedi Temple</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
