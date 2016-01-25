907082433ce08817734f15d0c5b251c9f93d65ed
Christian Mello <ecksian@icloud.com>
2016-01-24 18:05:33 -0800

    Added dummy text to display_articles.php including example website

---
+++ display_articles.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Display Articles</title>
+  </head>
+  <body>
+    <h3>Current Articles</h3>
+    <form class="" action="index.html" method="post">
+      <a href="http://www.cisco.com/cisco/web/solutions/small_business/resource_center/articles/secure_my_business/what_is_network_security/index.html">What Is Network Security?</a><br>
+      <label for="author">Author: Cisco Systems, Inc.</label><br>
+      <label for="category">Category: Network Security</label><br>
+      <label for="date">Date: 1/28/2015</label></br>
+      <label for="force">Midi-Chlorian Count: 9001</label></br>
+      <button type="upvote" name="upvote">Upvote</button>
+    </form>
+  </body>
+</html>
