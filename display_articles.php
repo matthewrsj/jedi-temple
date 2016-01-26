958e2374003a184ad895f10c147beeb8e9ab4027
Cody Malick <cody.malick@gmail.com>
2016-01-25 17:54:48 -0800

    bootstrapped display articles.php

--- display_articles.php
+++ display_articles.php
@@ -1,11 +1,26 @@
 <?php include('header.php') ?>
-    <h3>Current Articles</h3>
-    <form class="" action="index.html" method="post">
-      <a href="http://www.cisco.com/cisco/web/solutions/small_business/resource_center/articles/secure_my_business/what_is_network_security/index.html">What Is Network Security?</a><br>
-      <label for="author">Author: Cisco Systems, Inc.</label><br>
-      <label for="category">Category: Network Security</label><br>
-      <label for="date">Date: 1/28/2015</label></br>
-      <label for="force">Midi-Chlorian Count: 9001</label></br>
-      <button type="upvote" name="upvote">Upvote</button>
-    </form>
+  <div class="container">
+    <div class="row">
+      <h3>Current Articles</h3>
+    </div>
+    <div class="row">
+      <div class="jumbotron">
+        <form class="" action="index.html" method="post">
+          <div class="form-group">
+            <a href="http://www.cisco.com/cisco/web/solutions/small_business/resource_center/articles/secure_my_business/what_is_network_security/index.html">What Is Network Security?</a><br>
+            <label for="author">Author: Cisco Systems, Inc.</label><br>
+            <label for="category">Category: Network Security</label><br>
+            <label for="date">Date: 1/28/2015</label></br>
+            <label for="force">Midi-Chlorian Count: 9001</label></br>
+          </div>
+          <div class="form-group">
+            <button class="btn btn-default" type="upvote" name="upvote">Upvote</button>            
+          </div>
+        </form>
+      </div>
+    </div>
+
+
+  </div>
+
 <?php include('footer.php') ?>
