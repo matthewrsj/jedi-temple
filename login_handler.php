464d36a47a6bbf882eb15632b42490446022db3f
Matthew Johnson <mrsj@osuosl.org>
2016-01-27 15:59:04 -0800

    remove login handler

--- login_handler.php
+++
@@ -1,6 +0,0 @@
-<?php 
-function cas_authentication($ticket){
-   $cas_response = http_get("https://login.oregonstate.edu/cas/serviceValidate?ticket=" . $ticket . "&service=http://web.engr.oregonstate.edu/~malickc/");
-   echo $cas_response
-}
-?>
