08746a5ef396b36dac4d366a033b21bf687c2597
Matthew Johnson <mrsj@osuosl.org>
2016-01-28 09:00:35 -0800

    Meaningful(ish) footer

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
   <footer class="footer">
     <div class="container">
       <p class="text-muted">
-        This is the footer
+        Matthew Johnson, Cody Malick, Kenny Steinfeldt, Jacob Broderick, Christian Mello
       </p>
     </div>
   </footer>
