6e99cb3a6a28f4217166d9d5308a4c9eeb60d685
Kenneth Steinfeldt <steinfek@oregonstate.edu>
2016-01-27 19:04:32 -0800

    Data display and some things cause outage.

--- display_articles.php
+++ display_articles.php
@@ -6,16 +6,54 @@
     <div class="row">
       <div class="jumbotron">
         <form class="" action="index.html" method="post">
-          <div class="form-group">
-            <a href="http://www.cisco.com/cisco/web/solutions/small_business/resource_center/articles/secure_my_business/what_is_network_security/index.html">What Is Network Security?</a><br>
-            <label for="author">Author: Cisco Systems, Inc.</label><br>
-            <label for="category">Category: Network Security</label><br>
-            <label for="date">Date: 1/28/2015</label></br>
-            <label for="force">Midi-Chlorian Count: 9001</label></br>
-          </div>
-          <div class="form-group">
-            <button class="btn btn-default" type="upvote" name="upvote">Upvote</button>            
-          </div>
+          <?php
+            $dbhost = 'oniddb.cws.oregonstate.edu';
+            $dbname = 'malickc-db';
+            $dbuser = 'malickc-db';
+            $dbpass = 'Jz8QJFUt65lTYY16';
+
+            $mysql_handle = mysql_connect($dbhost, $dbuser, $dbpass)
+                or die("Error connecting to database server");
+
+            mysql_select_db($dbname, $mysql_handle)
+                or die("Error selecting database: $dbname");
+
+            $query = "SELECT articles.title, articles.url, articles.user_id, articles.category_id,
+                articles.midichlorians, users.username, categories.name, articles.time_submitted
+                FROM articles, users, categories
+                WHERE articles.user_id = users.id AND articles.category_id = categories.id
+                ORDER BY articles.time_submitted DESC
+                LIMIT 5";
+            $articles = mysql_query($query);
+              while($row = mysql_fetch_array($articles)) {
+            echo "
+          <div class='container'>
+            <div class='row'>
+              <h5><a href='" . $row["url"] . "'>" . $row['title'] . "</a> - " .
+                $row["midichlorians"] . " </h5>
+              <p><h6>Users:" . $row["username"] . "</h6></p>
+              <p><h6>" . $row["name"] . "</h6></p>
+              ";
+              if (checkAuth(false) != "") {
+              echo "
+              <div class='form-group'>
+
+                <button class='btn btn-default' type='upvote' name='upvote'>Upvote</button>
+
+                <button class='btn btn-default' type='downvote' name='downvote'>Downvote</button>
+                  </div>
+                  </div>
+                  </div>
+                  <hr>
+                ";
+                }
+              else {
+                echo "Log in to vote on this article.<hr>";
+                }
+              }
+
+          mysql_close($mysql_handle);
+          ?>
         </form>
       </div>
     </div>
--- header.php
+++ header.php
@@ -1,3 +1,39 @@
+<?php
+session_start();
+
+function checkAuth($doRedirect) {
+  if (isset($_SESSION["onidid"]) && $_SESSION["onidid"] != "") return $_SESSION["onidid"];
+
+   $pageURL = 'http';
+   if (isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] == "on") {$pageURL .= "s";}
+   $pageURL .= "://";
+   if ($_SERVER["SERVER_PORT"] != "80") {
+    $pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["SCRIPT_NAME"];
+   } else {
+    $pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["SCRIPT_NAME"];
+   }
+
+  $ticket = isset($_REQUEST["ticket"]) ? $_REQUEST["ticket"] : "";
+
+  if ($ticket != "") {
+    $url = "https://login.oregonstate.edu/cas/serviceValidate?ticket=".$ticket."&service=".$pageURL;
+    $html = file_get_contents($url);
+    $pattern = '/\\<cas\\:user\\>([a-zA-Z0-9]+)\\<\\/cas\\:user\\>/';
+    preg_match($pattern, $html, $matches);
+    if ($matches && count($matches) > 1) {
+      $onidid = $matches[1];
+      $_SESSION["onidid"] = $onidid;
+      return $onidid;
+    }
+  } else if ($doRedirect) {
+    $url = "https://login.oregonstate.edu/cas/login?service=".$pageURL;
+    echo "<script>location.replace('" . $url . "');</script>";
+  }
+  return "";
+}
+
+?>
+
 <!DOCTYPE html>
 <html>
   <head>
@@ -11,6 +47,7 @@
     <!-- Latest compiled JavaScript -->
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
     <title>Jedi Temple</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
   </head>
 
   <nav class="navbar navbar-default">
