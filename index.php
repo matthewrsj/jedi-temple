78b6313e5d0a137403dadb69f4abbc0e4e4d6932
Kenneth Steinfeldt <steinfek@oregonstate.edu>
2016-01-31 18:51:36 -0800

    index.php wasn't pulling top contributors

--- index.php
+++ index.php
@@ -92,6 +92,38 @@
       </div>
       <div class="col-md-3">
         <label for="">Most Powerful Contributors</label>
+          <?php
+
+            $dbhost = 'oniddb.cws.oregonstate.edu';
+            $dbname = 'malickc-db';
+            $dbuser = 'malickc-db';
+            $dbpass = 'Jz8QJFUt65lTYY16';
+
+
+            // Create connection
+            $mysql_handle = mysql_connect($dbhost, $dbuser, $dbpass)
+                or die("Error connecting to database server");
+
+            mysql_select_db($dbname, $mysql_handle)
+                or die("Error selecting database: $dbname");
+
+            $query = "SELECT * FROM users ORDER BY midichlorians DESC LIMIT 5";
+            $contributors = mysql_query($query);
+
+            // output data of each row
+            while($row = mysql_fetch_array($contributors)) {
+                echo "
+              <div class='container'>
+                <div class='row'>
+                  <h4>".$row["username"]."</h4>
+                  <p><h6>Midichlorians: ".$row["midichlorians"]."</h6></p>
+                  </div>
+                </div>";
+            }
+
+            mysql_close($mysql_handle);
+
+            ?>
       </div>
     </div>
   </div>
