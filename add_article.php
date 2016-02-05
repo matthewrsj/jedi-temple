776d670fd8e4b95b3bc3781529816e3135b54828
Matthew Johnson <mrsj@osuosl.org>
2016-02-04 16:20:29 -0800

    Validate the url with the html url input type
    Fixes #80

--- add_article.php
+++ add_article.php
@@ -12,11 +12,7 @@
             </div>
             <div class="form-group">
               <label for="url">Link to Article</label>
-        <?php if($_SESSION['is_url'] == false){
-            echo "<h6 style='color:red'>Must input a valid url</h6>";
-            $_SESSION['is_url'] == true;
-           } ?>
-              <input class="form-control" type="textbox" id="url" name="url" value="" placeholder="http://example.com/sweet-article" required>
+              <input class="form-control" type="url" id="url" name="url" value="" placeholder="http://example.com/sweet-article" required>
             </div>
             <div class="form-group">
               <label for="category_id">Category</label>
