3cc899416083528932ac3902ec8afa5669dee414
Christian Mello <ecksian@icloud.com>
2016-02-04 21:20:39 -0800

    Attempted change of dropdown

--- categorize_articles.php
+++ categorize_articles.php
@@ -30,7 +30,7 @@ function upvote(id){
 					?>
 					<div class="dropdown">
 					  <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-					    Dropdown
+					    <?php if($selected = 1) { echo "Network Security"; } ?>
 					    <span class="caret"></span>
 					  </button>
 					  <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
