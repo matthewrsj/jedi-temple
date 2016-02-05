5cd4c5bfb09a09262cdb58f2d45f2db0c30b71e9
Christian Mello <ecksian@icloud.com>
2016-02-05 00:16:47 -0800

    Reverted SQL changes...

--- categorize_articles.php
+++ categorize_articles.php
@@ -98,7 +98,16 @@ function upvote(id){
         <div class="row">
             <div class="list-group">
                 <?php
-                    
+                    $dbhost = 'oniddb.cws.oregonstate.edu';
+                    $dbname = 'malickc-db';
+                    $dbuser = 'malickc-db';
+                    $dbpass = 'Jz8QJFUt65lTYY16';
+                    $num_rec_per_page = 5;
+                    $mysql_handle = mysql_connect($dbhost, $dbuser, $dbpass)
+                        or die("Error connecting to database server");
+
+                    mysql_select_db($dbname, $mysql_handle)
+                        or die("Error selecting database: $dbname");
                     if (isset($_GET["page"])) { $page  = $_GET["page"]; } else { $page=1; };
                     if (isset($_GET["category"])) { $category = $_GET["category"]; } else { $category = 0; };
                     $start_from = ($page-1) * $num_rec_per_page;
